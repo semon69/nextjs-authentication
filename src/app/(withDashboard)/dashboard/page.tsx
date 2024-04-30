fd1911ac9d8373626b8cfacdc0647960e8fa7fea
unknown <smdemon3@gmail.com>
2024-04-30 16:10:49 +0600

    create github login

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,26 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions)
   return (
-    <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+    <div className="text-center">
+      {session?.user && (
+        <>
+          <h1 className="text-4xl  mt-10">Welcome: {session?.user?.name}</h1>
+
+          <h1 className="text-3xl mt-10">Your Email: {session?.user?.email}</h1>
+
+          <Image
+            src={session?.user?.image as string}
+            alt="next image"
+            width={200}
+            height={200}
+            className="mx-auto rounded-full"
+          />
+        </>
+      )}
     </div>
   );
 };
